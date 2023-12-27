8e685735a5289da51184d9bb15dd1877a58bf2ff
Nemanja Ćinćur <ncincur@gmail.com>
2023-12-28 00:28:48 +0100

    Second commit, task B

--- ProjectApp/Controllers/HomeController.cs
+++ ProjectApp/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using ProjectApp.DTO;
 using ProjectApp.Models;
+using SkiaSharp;
 using System.Diagnostics;
+using System.Drawing;
 using System.Net.Http;
 
 namespace ProjectApp.Controllers
@@ -32,8 +34,97 @@ namespace ProjectApp.Controllers
                                            TotalHours = x.Sum(x => x.TotalHours)
                                        }).ToList();
 
+            var image = DrawPieChart(grouped);
+
+            var imagePath = Path.Combine("wwwroot", "images", "chart.png");
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                image.Encode(SKEncodedImageFormat.Png, 100).SaveTo(stream);
+            }
+
+            ViewBag.ImagePath = imagePath;
 
             return View(grouped);
+        }
+
+        private SKBitmap DrawPieChart(List<EmployeeDTO> employeeDataList)
+        {
+            var width = 400;
+            var height = 400;
+
+            using (var surface = SKSurface.Create(width, height, SKImageInfo.PlatformColorType, SKAlphaType.Premul))
+            {
+                var canvas = surface.Canvas;
+                canvas.Clear(SKColors.White);
+
+                var totalHours = employeeDataList.Sum(e => e.TotalHours);
+
+                var startAngle = 0.0f;
+
+                var colors = new SKColor[]
+                {
+            SKColors.Blue,
+            SKColors.Red,
+            SKColors.Yellow,
+            SKColors.Green,
+            SKColors.Orange,
+                };
+
+                var colorIndex = 0;
+
+                foreach (var employee in employeeDataList)
+                {
+                    var percentage = (float)(100.0 * employee.TotalHours / totalHours);
+                    var sweepAngle = (float)(360.0 * percentage / 100);
+
+                    using (var paint = new SKPaint
+                    {
+                        Style = SKPaintStyle.Fill,
+                        Color = colors[colorIndex],
+                    })
+                    {
+                        canvas.DrawArc(new SKRect(0, 0, width, height), startAngle, sweepAngle, true, paint);
+
+                        var textPaint = new SKPaint
+                        {
+                            TextSize = 24.0f,
+                            Color = SKColors.Black,
+                            TextAlign = SKTextAlign.Center,
+                            IsAntialias = true,
+                        };
+
+                        var textAngle = startAngle + sweepAngle / 2.0f;
+                        var textRadius = width / 2.5f;
+
+                        var x = width / 2.0f + textRadius * (float)Math.Cos(Math.PI * textAngle / 180.0);
+                        var y = height / 2.0f + textRadius * (float)Math.Sin(Math.PI * textAngle / 180.0);
+
+                        canvas.DrawText(employee.EmployeeName, x, y, textPaint);
+
+                        var percentageTextPaint = new SKPaint
+                        {
+                            TextSize = 16.0f,
+                            Color = SKColors.Black,
+                            TextAlign = SKTextAlign.Center,
+                            IsAntialias = true,
+                        };
+
+                        var percentageX = width / 2.0f + textRadius * (float)Math.Cos(Math.PI * textAngle / 180.0);
+                        var percentageY = height / 2.0f + textRadius * (float)Math.Sin(Math.PI * textAngle / 180.0) + 20.0f;
+
+                        canvas.DrawText($"{percentage:F2}%", percentageX, percentageY, percentageTextPaint);
+                    }
+
+                    startAngle += sweepAngle;
+
+                    colorIndex = (colorIndex + 1) % colors.Length;
+                }
+
+                using (var skiaImage = surface.Snapshot())
+                {
+                    return SKBitmap.FromImage(skiaImage);
+                }
+            }
         }
 
         public IActionResult Privacy()
